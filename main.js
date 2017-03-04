08081d7dae917ae0ef36121676095aaefb149fd9
SteveSu55 <steve20130309@gmail.com>
2017-03-04 20:33:51 +0800

    enemy

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var FPS = 60;
+var clock = 0;
 var bgImg = document.createElement("img");
 var enemyImg = document.createElement("img");
 var btnImg = document.createElement("img");
@@ -6,7 +7,8 @@ var towerImg = document.createElement("img");
 
 // 設定這個元素的要顯示的圖片
 bgImg.src = "images/map.png";
-enemyImg.src = "images/slime.gif";
+enemyImg.src = "images/jason.gif";
+enemyImg.src = "images/rukia.gif";
 btnImg.src = "images/tower-btn.png";
 towerImg.src = "images/tower.png";
 
@@ -17,10 +19,19 @@ var canvas = document.getElementById("game-canvas");
 var ctx = canvas.getContext("2d");
 
 function draw(){
-	enemy.move()
+	clock++;
+	if (clock%50 == 0) {
+		var newEnemy = new Enemy();
+		enemies.push(newEnemy);
+	}
+	
 	// 將背景圖片畫在 canvas 上的 (0,0) 位置
 	ctx.drawImage(bgImg,0,0);
-	ctx.drawImage(enemyImg,enemy.x,enemy.y);
+	for(var i = 0;i<enemies.length;i++){
+		enemies[i].move()
+		ctx.drawImage(enemyImg,enemies[i].x,enemies[i].y);
+	}
+	
 	ctx.drawImage(btnImg,640-64,480-64,64,64)
 	if(isBuilding ==true){
     ctx.drawImage(towerImg,cursor.x,cursor.y)
@@ -43,13 +54,13 @@ var enemyPath =  [
 ]
 
 
-var enemy = {
-	x: 96,
-	y: 480-32,
-	speedX: 0,
-	speedY: -64,
-	pathDes: 0,
-	move: function(){
+function Enemy () {
+	this.x= 96;
+	this.y= 480-32;
+	this.speedX= 0;
+	this.speedY= -64;
+	this.pathDes= 0;
+	this.move= function(){
 		console.log(this.y)
 		console.log(isCollided(enemyPath[this.pathDes].x,enemyPath[this.pathDes].y,this.x,this.y, 64/FPS, 64/FPS))
 		if(isCollided(enemyPath[this.pathDes].x,enemyPath[this.pathDes].y,this.x,this.y, 64/FPS, 64/FPS)){
@@ -77,6 +88,7 @@ var enemy = {
 		
 	}
 }
+var enemies = []
 
 var cursor = {
 	x:100,
